29eab5d5a6206f6d162ab80a3e07786d057b1f7c
alpesh_pandya <alpesh_pandya@apple.com>
2018-06-02 20:05:44 -0500

    setup and requirement changes

--- src/setup.sh
+++ src/setup.sh
@@ -2,7 +2,7 @@
 virtualenv python-ci-cd-env
 chmod 0755 python-ci-cd-env/bin/activate
 script_dir=`dirname $0`
-/bin/bash -c ". python-ci-cd-env/bin/activate"
+source python-ci-cd-env/bin/activate
 which python
 pip install -r src/requirements.txt
 deactivate
